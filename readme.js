d9f341d8d8ed23b1ba187bd6143b3b19024f4dbe
redcartel <carteradams@gmail.com>
2024-02-04 13:18:15 -0500

    rm readme.js

--- readme.js
+++
@@ -1,8 +0,0 @@
-import DataLoaderRedis from "src/";
-import { createClient } from "redis";
-
-const loader = new DataLoaderRedis(createClient(), async (keys) => {
-    return keys.map(key => new Promise((res) => setTimeout(()=>res(key))));
-})
-
-// ...
